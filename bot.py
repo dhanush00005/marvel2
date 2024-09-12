64632fb5a508eae0374c0e9b401f2f534e6d9b58
dhanush00005 <122075054+dhanush00005@users.noreply.github.com>
2024-09-12 12:40:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,18 +63,18 @@ async def op(_, m :Message):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/vjmasterblastbot?startgroup")
+                    InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/VINAYAKAPREDICTIONS_bot?startgroup")
                 ]
             ]
         )
         add_user(m.from_user.id)
-        await m.reply_photo("h", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @VJ_Botz __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+        await m.reply_photo("h", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By Hanuman Predictions**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
 
     elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
         keyboar = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("💁‍♂️ Start me private 💁‍♂️", url="https://t.me/?startgroup")
+                    InlineKeyboardButton("💁‍♂️ Start me private 💁‍♂️", url="https://t.me/VINAYAKAPREDICTIONS_bot?startgroup")
                 ]
             ]
         )
@@ -111,7 +111,7 @@ async def chk(_, cb : CallbackQuery):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/vjmasterblastbot?startgroup")
+                    InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/VINAYAKAPREDICTIONS_bot?startgroup")
                 ]
             ]
         )
