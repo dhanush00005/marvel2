e41439dd79d4beda0a52e616083dfa230070ca69
dhanush00005 <122075054+dhanush00005@users.noreply.github.com>
2024-09-12 16:00:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "6941310088:AAFFPIbx5pJrm91U_yu4ebT5nDuS0_Uh-0Q")
     SESSION_STRING = getenv("SESSION_STRING", "BQD6zg0AkteC-fzmT32_UFWFwlyxIL1UeP187MqL7Vty74xeg9F5WOnXM3o5hv45l_sNznIHBvxm18vWFEF038qDS6EqnS7QwwaSNm--lZT4DoT-cDhRNn2AGBLSXBTgpmPx9CJT6jfstBHA9nYCfQ3-PMWFH9gofNfMQl2YR74DdqveVwHaZC5WkqA55Mckfnd5-AyIsdIYJPNv3R9GQVcQSkK0ftl8rXDqc3Rl7RmbBtCxpZlIIAyfB9GNhbvQBoK7GalEoStcJjKfeq7-ETbbwtjCsotDvhVKYPABeyX3jNALWJJdFYXHB2M4wm35A1bLzXQ7oSrjXNNm_ZR5g9sR9Lf5UgAAAAFry-lQAA")
     SUDO = list(map(int, getenv("SUDO", "5798247275 1397661911").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Sai6211:Sai@6211@cluster0.0kxm7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sai1422:sai1422@cluster0.jxlw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 cfg = Config()
 
