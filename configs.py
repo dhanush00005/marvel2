c7d0e08e0a73c69919f9455105c5c18fdec80f30
dhanush00005 <122075054+dhanush00005@users.noreply.github.com>
2024-09-12 12:26:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,10 +8,10 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "20389440"))
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6523760901:AAEdPrDJXZTOmZS3G7MxFidl_HrBXyyTmRQ")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6941310088:AAFFPIbx5pJrm91U_yu4ebT5nDuS0_Uh-0Q")
     SESSION_STRING = getenv("SESSION_STRING", "BQD6zg0AkteC-fzmT32_UFWFwlyxIL1UeP187MqL7Vty74xeg9F5WOnXM3o5hv45l_sNznIHBvxm18vWFEF038qDS6EqnS7QwwaSNm--lZT4DoT-cDhRNn2AGBLSXBTgpmPx9CJT6jfstBHA9nYCfQ3-PMWFH9gofNfMQl2YR74DdqveVwHaZC5WkqA55Mckfnd5-AyIsdIYJPNv3R9GQVcQSkK0ftl8rXDqc3Rl7RmbBtCxpZlIIAyfB9GNhbvQBoK7GalEoStcJjKfeq7-ETbbwtjCsotDvhVKYPABeyX3jNALWJJdFYXHB2M4wm35A1bLzXQ7oSrjXNNm_ZR5g9sR9Lf5UgAAAAFry-lQAA")
-    SUDO = list(map(int, getenv("SUDO", "5798247275").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    SUDO = list(map(int, getenv("SUDO", "5798247275 1397661911").split()))
+    MONGO_URI = getenv("MONGO_URI", "")
 
 cfg = Config()
 
